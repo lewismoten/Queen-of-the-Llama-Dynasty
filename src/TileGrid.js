81897d57cbbc89dff351c22987dd1feb92c9d668
Lewis Moten <lewismoten@gmail.com>
2021-08-28 01:24:37 -0400

    add request animation frame

--- src/TileGrid.js
+++ src/TileGrid.js
@@ -1,20 +1,34 @@
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 const TileGrid = () => {
 
-  let ref = useRef(null);
+  const canvasRef = useRef(null);
+  const drawRequest = useRef();
+  const lastDrawn = useRef(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
-  useEffect(() => {
-    draw();
-  }, [])
+  const draw = time => {
+    if(canvasRef.current) {
+      const delta = time - lastDrawn.current;
 
-  const draw = () => {
-    const ctx = ref.current.getContext('2d');
-    ctx.fillStyle = '#440044';
-    ctx.fillRect(0, 0, 50, 50);
+      const ctx = canvasRef.current.getContext('2d');
+      ctx.fillStyle = '#440044';
+      ctx.fillRect(0, 0, 50, 50);
+
+      lastDrawn.current = time;
+    }
+    drawRequest.current = requestAnimationFrame(draw);
   }
 
-  return <canvas ref={ref} width="100%" height="100%" />
-}
+  useEffect(() => {
+    drawRequest.current = requestAnimationFrame(draw);
+    return () => cancelAnimationFrame(drawRequest.current);
+  }, [])
+
+
+
+    return <canvas ref={canvasRef} width="100%" height="100%" />
+  }
 
 export default TileGrid;
