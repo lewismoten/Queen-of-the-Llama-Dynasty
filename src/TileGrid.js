e02feeb8fe304c4737215227ada1671709bcd9ef
Lewis Moten <lewismoten@gmail.com>
2021-08-28 13:28:46 -0400

    move hero square with arrow keys

--- src/TileGrid.js
+++ src/TileGrid.js
@@ -1,32 +1,84 @@
-import {useEffect, useRef, useState} from 'react';
+import {useEffect, useRef, useState, useCallback} from 'react';
+
+const KEY_CODE = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
+
+const HERO = {
+  WIDTH: 10,
+  HEIGHT: 10
+}
+
+const move = ({x,y}, speed, input) => {
+  y = input.includes(KEY_CODE.DOWN) ? y + speed : input.includes(KEY_CODE.UP) ? y - speed : y;
+  x = input.includes(KEY_CODE.LEFT) ? x - speed : input.includes(KEY_CODE.RIGHT) ? x + speed : x;
+  y = Math.round(y * 10) / 10;
+  x = Math.round(x * 10) / 10;
+  return {x,y};
+}
 
 const TileGrid = () => {
 
+
   const canvasRef = useRef(null);
-  const drawRequest = useRef();
-  const lastDrawn = useRef(0);
+  const frameRequest = useRef();
+  const lastFrameTime = useRef(0);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
+  const [input, setInput] = useState([]);
 
-  const draw = time => {
-    if(canvasRef.current) {
-      const delta = time - lastDrawn.current;
+  const onFrame = time => {
+    if(!canvasRef.current) return;
 
-      const ctx = canvasRef.current.getContext('2d');
-      ctx.fillStyle = '#440044';
-      ctx.fillRect(0, 0, 50, 50);
+    const delta = (time - lastFrameTime.current) / 50;
 
-      lastDrawn.current = time;
-    }
-    drawRequest.current = requestAnimationFrame(draw);
-  }
+    const point = move({x,y}, delta, input);
+    if(x !== point.x || y !== point.y)
+      console.log(point);
+    if(x !== point.x) setX(point.x);
+    if(y !== point.y) setY(point.y);
+
+    const ctx = canvasRef.current.getContext('2d');
+
+    // background
+    ctx.fillStyle = '#440044';
+    ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+    // hero
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(point.x, point.y, HERO.WIDTH, HERO.HEIGHT);
+
+    lastFrameTime.current = time;
+
+    frameRequest.current = requestAnimationFrame(onFrame);
+  };
+
+  const onKeyDown = ({keyCode}) => {
+    if(!input.includes(keyCode)) setInput([...input, keyCode]);
+  };
+  const onKeyUp = ({keyCode}) => {
+    if(input.includes(keyCode)) setInput(input.filter(v => v !== keyCode))
+  };
+
+  const onKeyDownCallback = useCallback(onKeyDown, [input]);
+  const onKeyUpCallback = useCallback(onKeyUp, [input]);
+  const onFrameCallback = useCallback(onFrame, [x, y, input])
 
   useEffect(() => {
-    drawRequest.current = requestAnimationFrame(draw);
-    return () => cancelAnimationFrame(drawRequest.current);
-  }, [])
-
-
+    frameRequest.current = requestAnimationFrame(onFrameCallback);
+    const keyOptions = {capture: false};
+    // multiple keys down at the same time freezing logic
+    window.addEventListener("keydown", onKeyDownCallback, keyOptions);
+    window.addEventListener("keyup", onKeyUpCallback, keyOptions);
+    return () => {
+      cancelAnimationFrame(frameRequest.current);
+      window.removeEventListener("keydown", onKeyDownCallback, keyOptions);
+      window.removeEventListener("keyup", onKeyUpCallback, keyOptions);
+    }
+  }, [onFrameCallback, onKeyDownCallback, onKeyUpCallback]);
 
     return <canvas ref={canvasRef} width="100%" height="100%" />
   }
